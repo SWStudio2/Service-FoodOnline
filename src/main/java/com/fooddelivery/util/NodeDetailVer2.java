3c8bcfb8c801c72085b61023d8a8eac53de74d4e
lvlikeky <mike_foreverlove@hotmail.com>
2017-03-19 17:19:07 +0700

    add nodeDetail2
    add nodeDetail2

--- src/main/java/com/fooddelivery/util/NodeDetailVer2.java
+++ src/main/java/com/fooddelivery/util/NodeDetailVer2.java
@@ -54,49 +54,49 @@ public class NodeDetailVer2 {
 		this.longtitudeDelivery = longtitudeDelivery;
 	}
 	
-//	public static NodeDetailVer2 getBestNodeDetail(ArrayList<NodeDetailVer2> arrNode,TimeAndDistanceDetail[] tmpTimeDisDetail)
-//	{
-//		NodeDetailVer2 tmpNode;
-//		HomeController home = new HomeController();
-//		int bestTime = 9999;
-//		NodeDetailVer2 bestNode = null;
-//		int sumAll = 0;
-//		for(int i = 0;i<arrNode.size();i++)
-//		{
-//			int firstNodeTime = 0;
-//			sumAll = 0;
-//			tmpNode = (NodeDetailVer2)arrNode.get(i);
-//			Merchants firstMerchant = tmpNode.merList.get(0);
-//			String[] 
-//			firstNodeTime = Integer.parseInt(arrDetail[1]);
-//			
-//			int sumMerChantTime = 0;
-//			if(tmpNode.merList.size() > 1)
-//			{
-//				for(int j = 1;j<tmpNode.merList.size();j++)
-//				{
-//					Merchants merStart = tmpNode.merList.get(j-1);
-//					Merchants merEnd = tmpNode.merList.get(j);
-//					arrDetail = (String[])home.getDistanceDuration(merStart.getMerLatitude(), merStart.getMerLongtitude(), merEnd.getMerLatitude(), merEnd.getMerLongtitude());
-//					
-//					sumMerChantTime = Integer.parseInt(arrDetail[1]) + sumMerChantTime;
-//				}								
-//			}
-//			Merchants endMerchant = tmpNode.merList.get(tmpNode.merList.size()-1);
-//			int merchantToCusTime = 0;
-//			arrDetail = (String[])home.getDistanceDuration(endMerchant.getMerLatitude(), endMerchant.getMerLongtitude(), tmpNode.latitudeDelivery, tmpNode.longtitudeDelivery);
-//			merchantToCusTime = Integer.parseInt(arrDetail[1]);
-//			
-//
-//			sumAll = firstNodeTime + sumMerChantTime + merchantToCusTime;
-//			if(bestTime > sumAll)
-//			{
-//				bestNode = tmpNode;
-//				bestTime = sumAll;
-//			}
-//		}
-//		System.out.println("bestTime " + bestTime);
-//
-//		return bestNode;
-//	}
+	public static NodeDetailVer2 getBestNodeDetail(ArrayList<NodeDetailVer2> arrNode,TimeAndDistanceDetail[] tmpTimeDisDetail)
+	{
+		NodeDetailVer2 tmpNode;
+		HomeController home = new HomeController();
+		int bestTime = 9999;
+		NodeDetailVer2 bestNode = null;
+		int sumAll = 0;
+		for(int i = 0;i<arrNode.size();i++)
+		{
+			int firstNodeTime = 0;
+			sumAll = 0;
+			tmpNode = (NodeDetailVer2)arrNode.get(i);
+			Merchants firstMerchant = tmpNode.merList.get(0);
+			String[] timeAndDistance = tmpNode.
+			firstNodeTime = Integer.parseInt(arrDetail[1]);
+			
+			int sumMerChantTime = 0;
+			if(tmpNode.merList.size() > 1)
+			{
+				for(int j = 1;j<tmpNode.merList.size();j++)
+				{
+					Merchants merStart = tmpNode.merList.get(j-1);
+					Merchants merEnd = tmpNode.merList.get(j);
+					arrDetail = (String[])home.getDistanceDuration(merStart.getMerLatitude(), merStart.getMerLongtitude(), merEnd.getMerLatitude(), merEnd.getMerLongtitude());
+					
+					sumMerChantTime = Integer.parseInt(arrDetail[1]) + sumMerChantTime;
+				}								
+			}
+			Merchants endMerchant = tmpNode.merList.get(tmpNode.merList.size()-1);
+			int merchantToCusTime = 0;
+			arrDetail = (String[])home.getDistanceDuration(endMerchant.getMerLatitude(), endMerchant.getMerLongtitude(), tmpNode.latitudeDelivery, tmpNode.longtitudeDelivery);
+			merchantToCusTime = Integer.parseInt(arrDetail[1]);
+			
+
+			sumAll = firstNodeTime + sumMerChantTime + merchantToCusTime;
+			if(bestTime > sumAll)
+			{
+				bestNode = tmpNode;
+				bestTime = sumAll;
+			}
+		}
+		System.out.println("bestTime " + bestTime);
+
+		return bestNode;
+	}
 }
