5295a45d6946de70f61e526893cfaedaaaae416d
Differr <yui018@hotmail.com>
2017-03-19 19:47:13 +0700

    update MerchantsDao

--- src/main/java/com/fooddelivery/Controller/MessengerController.java
+++ src/main/java/com/fooddelivery/Controller/MessengerController.java
@@ -1,5 +1,10 @@
 package com.fooddelivery.Controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -202,8 +207,8 @@ public class MessengerController {
 			System.out.println();
 		}
 		
-		MerchantsDao merDao = new MerchantsDao();
-		Merchants[] merList = merDao.queryMerChantByID(merId);
+		
+		Merchants[] merList = queryMerChantByID(merId);
 		ArrayList<Integer> indexPos = new ArrayList<Integer>();
 		for(int i = 0;i<merId.length;i++)
 		{
@@ -268,6 +273,97 @@ public class MessengerController {
 		}
 		return idList;
 	}
+	
+	  public Merchants[] queryMerChantByID(String[] merId)
+	  {
+		  Connection connect = null;
+			Statement s = null;
+			String URL_DB = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_4e5de0567b1e3fc";
+			URL_DB = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_4e5de0567b1e3fc?useUnicode=true&amp;characterEncoding=UTF-8";
+			String USER = "ba8167e655c97d";
+			String PASSWORD = "fcc5664d";
+			
+			String merchantIdForQuery = "";
+			Merchants[] merList = null;
+			for(int i = 0;i<merId.length;i++)
+			{
+				if(i == 0)
+				{
+					merchantIdForQuery += merId[i];
+				}
+				else
+				{
+					merchantIdForQuery += "," + merId[i];
+				}
+			}
+			
+
+
+			// Close
+
+			
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connect = DriverManager.getConnection(URL_DB, USER, PASSWORD);
+				
+				if(connect != null){
+					System.out.println("Database Connected.");
+				} else {
+					System.out.println("Database Connect Failed.");
+				}
+				s = connect.createStatement();
+							
+				String sql = "";
+				
+				StringBuffer sqlBuffer = new StringBuffer();
+
+				sqlBuffer.append("SELECT MER_ID,MER_LATITUDE,MER_LONGTITUDE,MER_COOKTIME \n ");
+				sqlBuffer.append("FROM merchants  \n ");
+				sqlBuffer.append("Where mer_id in (merchantList) \n ");
+
+				
+				sql = sqlBuffer.toString();	
+				
+				ResultSet rec = s.executeQuery(sql);
+				
+				if(rec == null)
+				{
+					System.out.print(" record not found ");
+				}
+//				System.out.println("" + sql);
+				ArrayList<Merchants> arrMerchant = new ArrayList<Merchants>();
+				while((rec!=null) && (rec.next()))
+	            {
+					Merchants tmpMerchant = new Merchants();
+					tmpMerchant.setMerID(Long.parseLong(rec.getString("MER_ID")));
+					tmpMerchant.setMerLatitude(rec.getString("MER_LATITUDE"));
+					tmpMerchant.setMerLongtitude(rec.getString("MER_LONGTITUDE"));
+					tmpMerchant.setCookingTime(rec.getInt("MER_COOKTIME"));
+					arrMerchant.add(tmpMerchant);
+	            }
+				
+				merList = new Merchants[arrMerchant.size()];
+				for(int i = 0;i<arrMerchant.size();i++)
+				{
+					merList[i] = (Merchants)arrMerchant.get(i);
+				}
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			// Close
+			try {
+				if(connect != null){
+					connect.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}				  
+		  return merList;
+	  }
 	
 }
 
